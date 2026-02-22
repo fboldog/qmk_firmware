bf8ac7a2ba054e7324e3f3a1dc703036128d6a79
Ferenc Boldog <ferenc.boldog@gmail.com>
2026-02-23 00:19:53 +0100

    feature: rgb matrix, fully working, tested with solid_reactive_simple

--- keyboards/fboldog/open_lucky65v2/open_lucky65v2.c
+++ keyboards/fboldog/open_lucky65v2/open_lucky65v2.c
@@ -4,7 +4,7 @@
 #include "timer.h"
 
 static void change_rgb_matrix_state(bool enable);
-static void walk_red_leds(void);
+// static void walk_red_leds(void);
 
 #ifndef RGB_DRIVER_ENABLE_STATE
 #    define RGB_DRIVER_ENABLE_STATE 1
@@ -14,13 +14,13 @@ static void walk_red_leds(void);
 #    define RGB_DRIVER_DISABLE_STATE 1
 #endif
 
-#define MY_LED_COUNT RGB_MATRIX_LED_COUNT
-#define WALK_DELAY 5000
+// #define MY_LED_COUNT RGB_MATRIX_LED_COUNT
+// #define WALK_DELAY 5000
 
 
 // #ifdef CONSOLE_ENABLE
 void keyboard_post_init_kb(void) {
-    debug_enable   = true;
+    // debug_enable   = true;
     // debug_matrix   = false;
     // debug_keyboard = false;
 
@@ -66,10 +66,10 @@ bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
 
 // pre init
 void keyboard_pre_init_user(void) {
-    //  eeconfig_init();
-    //  eeconfig_update_rgb_matrix_default();
-    //  rgb_matrix_enable_noeeprom();
-    //  rgb_matrix_config.enable = true;
+     eeconfig_init();
+     eeconfig_update_rgb_matrix_default();
+     rgb_matrix_enable_noeeprom();
+     rgb_matrix_config.enable = true;
 #ifdef RGB_DRIVER_EN_PIN
     setPinOutput(RGB_DRIVER_EN_PIN);
 #endif
@@ -82,8 +82,8 @@ void keyboard_post_init_user(void) {
 }
 
 
-    static uint8_t  current    = 0;
-    static uint16_t last_step  = 0;
+    // static uint8_t  current    = 0;
+    // static uint16_t last_step  = 0;
 
 
 void change_rgb_matrix_state(bool enable) {
@@ -98,43 +98,43 @@ void change_rgb_matrix_state(bool enable) {
         // rgb_matrix_set_color(0, RGB_RED);
     } else {
             // Turn all LEDs off
-    for (uint8_t i = 0; i < MY_LED_COUNT; i++) {
-        rgb_matrix_set_color(i, RGB_OFF);
-    }
+    // for (uint8_t i = 0; i < MY_LED_COUNT; i++) {
+    //     rgb_matrix_set_color(i, RGB_OFF);
+    // }
         // writePin(RGB_DRIVER_EN_PIN, RGB_DRIVER_DISABLE_STATE);
         rgb_matrix_disable();
-    current = 0;
-    last_step = 0;
+    // current = 0;
+    // last_step = 0;
 
     }
 }
 
-void walk_red_leds(void) {
+// void walk_red_leds(void) {
 
-    // Initialize timer on first run
-    if (last_step == 0) {
-        last_step = timer_read();
-    }
+//     // Initialize timer on first run
+//     if (last_step == 0) {
+//         last_step = timer_read();
+//     }
 
-    // Wait until WALK_DELAY ms have passed
-    if (timer_elapsed(last_step) < WALK_DELAY) {
-        return;
-    }
-    last_step = timer_read();
+//     // Wait until WALK_DELAY ms have passed
+//     if (timer_elapsed(last_step) < WALK_DELAY) {
+//         return;
+//     }
+//     last_step = timer_read();
 
-    // Turn current LED on in red
-    uprintf("switching on: %d\n", current);
-    rgb_matrix_set_color(current, RGB_BLUE);
+//     // Turn current LED on in red
+//     uprintf("switching on: %d\n", current);
+//     rgb_matrix_set_color(current, RGB_BLUE);
 
-    // Advance to next LED with wraparound
-    current++;
-    if (current >= MY_LED_COUNT) {
-        current = 0;
-    }
-}
+//     // Advance to next LED with wraparound
+//     current++;
+//     if (current >= MY_LED_COUNT) {
+//         current = 0;
+//     }
+// }
 
-void matrix_scan_user(void) {
-    if (rgb_matrix_is_enabled()) {
-        walk_red_leds();
-    }
-}
+// void matrix_scan_user(void) {
+    // if (rgb_matrix_is_enabled()) {
+    //     walk_red_leds();
+    // }
+// }
