9af27079f37d1b0d7e8ab0f5d9e9ac65e6fb8842
Ferenc Boldog <ferenc.boldog@gmail.com>
2026-04-06 21:31:53 +0200

    feature: split space layout

--- keyboards/fboldog/open_lucky65v2/rules.mk
+++ keyboards/fboldog/open_lucky65v2/rules.mk
@@ -1,6 +1,10 @@
 #debug
-CONSOLE_ENABLE = yes
 KEYCODE_STRING_ENABLE = yes
+
+# for split space keyboard the shared USB endpoint is required to keep all features
+ifeq ($(KEYMAP), split)
+KEYBOARD_SHARED_EP = yes
+endif
 
 #rgb matrix
 WS2812_DRIVER_REQUIRED = yes
