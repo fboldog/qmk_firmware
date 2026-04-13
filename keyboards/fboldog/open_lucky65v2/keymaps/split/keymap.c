63b1af43b36f7b4236e32a0b728ce8bf13a81163
Ferenc Boldog <ferenc.boldog@gmail.com>
2026-04-13 16:45:01 +0200

    bugfix: use del and tilde/backtick on every layer

--- keyboards/fboldog/open_lucky65v2/keymaps/split/keymap.c
+++ keyboards/fboldog/open_lucky65v2/keymaps/split/keymap.c
@@ -13,24 +13,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL,    KC_LGUI, KC_LALT,     KC_SPC,           MO(1),           KC_SPC,          MO(3),   KC_RALT,  KC_LEFT,    KC_DOWN,    KC_RGHT
     ),
 
-        KC_GRV,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_TRNS,    KC_TRNS,
     [1] = LAYOUT_split( //MO(1)
+        KC_GRV,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_TRNS,    KC_DEL,
         RM_TOGG,  RM_ON,   RM_OFF,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  RM_VALD,  RM_VALU,  KC_TRNS,    KC_TRNS,
         KC_TRNS,  KC_TRNS,  KC_TRNS,  DB_TOGG,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  RM_PREV,  RM_NEXT,  KC_TRNS,              KC_TRNS,
         KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  EE_CLR,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,    KC_TRNS,
         KC_TRNS,  KC_TRNS,  KC_TRNS,         KC_TRNS,                       KC_TRNS,        KC_TRNS,                        KC_TRNS,  KC_RGUI,  KC_TRNS,  KC_TRNS,    KC_TRNS
     ),
 
-        KC_TRNS,   KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS,
     [2] = LAYOUT_split( //MO(2)
+        KC_GRV,   KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_DEL,
         KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,
         KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,              KC_TRNS,
         KC_TRNS,  KC_TRNS,  KC_TRNS, EE_CLR, KC_TRNS,  QK_BOOT,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,    KC_TRNS,
         KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,                    KC_TRNS,         KC_TRNS,                       KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS
     ),
 
-        KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_TRNS,    KC_TRNS,
     [3] = LAYOUT_split( //MEDIA (Mac) - FN3
+        KC_GRV,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_TRNS,    KC_DEL,
         KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,
         KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,    KC_TRNS,
         KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,    KC_TRNS,
