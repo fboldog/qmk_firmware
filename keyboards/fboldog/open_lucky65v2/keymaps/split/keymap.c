9d72036a60ddbd7faa640d378ebbd400f1da99c3
Ferenc Boldog <ferenc.boldog@gmail.com>
2026-04-13 16:44:27 +0200

    bugfix: simpler layout name (split instead of split_spc)

--- keyboards/fboldog/open_lucky65v2/keymaps/split/keymap.c
+++ keyboards/fboldog/open_lucky65v2/keymaps/split/keymap.c
@@ -5,7 +5,7 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-    [0] = LAYOUT_split_spc( //BASE
+    [0] = LAYOUT_split( //BASE
         KC_ESC,     KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,    KC_EQL,     KC_BSPC,    KC_HOME,
         KC_TAB,     KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,    KC_RBRC,    KC_BSLS,    KC_END,
         MO(2),    KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN, KC_QUOT,               KC_ENT,     KC_PGUP,
@@ -13,24 +13,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL,    KC_LGUI, KC_LALT,     KC_SPC,           MO(1),           KC_SPC,          MO(3),   KC_RALT,  KC_LEFT,    KC_DOWN,    KC_RGHT
     ),
 
-    [1] = LAYOUT_split_spc( //MO(1)
         KC_GRV,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_TRNS,    KC_TRNS,
+    [1] = LAYOUT_split( //MO(1)
         RM_TOGG,  RM_ON,   RM_OFF,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  RM_VALD,  RM_VALU,  KC_TRNS,    KC_TRNS,
         KC_TRNS,  KC_TRNS,  KC_TRNS,  DB_TOGG,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  RM_PREV,  RM_NEXT,  KC_TRNS,              KC_TRNS,
         KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  EE_CLR,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,    KC_TRNS,
         KC_TRNS,  KC_TRNS,  KC_TRNS,         KC_TRNS,                       KC_TRNS,        KC_TRNS,                        KC_TRNS,  KC_RGUI,  KC_TRNS,  KC_TRNS,    KC_TRNS
     ),
 
-    [2] = LAYOUT_split_spc( //MO(2)
         KC_TRNS,   KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS,
+    [2] = LAYOUT_split( //MO(2)
         KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,
         KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,              KC_TRNS,
         KC_TRNS,  KC_TRNS,  KC_TRNS, EE_CLR, KC_TRNS,  QK_BOOT,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,    KC_TRNS,
         KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,                    KC_TRNS,         KC_TRNS,                       KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS
     ),
 
-    [3] = LAYOUT_split_spc( //MEDIA (Mac) - FN3
         KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_TRNS,    KC_TRNS,
+    [3] = LAYOUT_split( //MEDIA (Mac) - FN3
         KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,
         KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,    KC_TRNS,
         KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,    KC_TRNS,
